6455a33899354d29d88212f1af0abc7034af61d0
Alan Szpigiel <alanszp@gmail.com>
2021-06-28 00:05:40 -0300

    generalize paginable to be able to validate on string & add mapListResult function

--- packages/core/src/lists/ListResult.ts
+++ packages/core/src/lists/ListResult.ts
@@ -4,3 +4,13 @@ export interface ListResult<Result> {
   page: number;
   pageSize: number;
 }
+
+export function mapListResult<T, R>(
+  list: ListResult<T>,
+  mapper: (elem: T) => R
+): ListResult<R> {
+  return {
+    ...list,
+    elements: list.elements.map((c) => mapper(c)),
+  };
+}
--- packages/core/src/lists/Paginable.ts
+++ packages/core/src/lists/Paginable.ts
@@ -1,11 +1,20 @@
-export interface Paginable {
-  pageSize: number;
-  page: number;
+export interface Paginable<Validated extends boolean = false> {
+  pageSize: Validated extends true ? number : number | string;
+  page: Validated extends true ? number : number | string;
 }
 
-export function getPageObject<T extends Paginable>(object: T) {
+export function getPageObject<T extends Paginable<true>>(object: T) {
   return {
     skip: (object.page - 1) * object.pageSize,
     take: object.pageSize,
+  };
+}
+
+export function extractPaginableParams<T extends Paginable>(
+  object: T
+): Paginable<true> {
+  return {
+    page: Number.parseInt(object.page as string, 10),
+    pageSize: Number.parseInt(object.pageSize as string, 10),
   };
 }
