b4d1bb8f151323dfaa6bc49b44c2908611bfeba0
Alan Szpigiel <alanszp@gmail.com>
2021-06-27 23:30:08 -0300

    add default value to assingKey

--- packages/core/src/assignKey.ts
+++ packages/core/src/assignKey.ts
@@ -1,11 +1,16 @@
-export function assignKey<CommonInterface>(
+export function assignKey<CommonInterface, Key extends keyof CommonInterface>(
   target: CommonInterface,
   source: CommonInterface,
-  key: keyof CommonInterface
-) {
+  key: Key,
+  defaultValue?: CommonInterface[Key]
+): void {
   if (source[key]) {
-    // eslint-disable-next-line no-param-reassign
     target[key] = source[key];
+    return;
+  }
+
+  if (defaultValue) {
+    target[key] = defaultValue;
   }
 }
 
@@ -13,7 +18,7 @@ export function assignKeys<CommonInterface>(
   target: CommonInterface,
   source: CommonInterface,
   keys: (keyof CommonInterface)[]
-) {
+): void {
   keys.forEach((key) => {
     assignKey(target, source, key);
   });
