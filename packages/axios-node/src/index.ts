4e2840bf29983e5a22a6a4be59f96c5a0880a0a2
Alan Szpigiel <alanszp@gmail.com>
2021-08-29 16:43:41 -0300

    export error

--- packages/axios-node/src/index.ts
+++ packages/axios-node/src/index.ts
@@ -1,1 +1,5 @@
 export * from "./axios";
+export * from "./errors/GenericError";
+export * from "./errors/NetworkRequestError";
+export * from "./errors/Non200ResponseError";
+export * from "./errors/RequestError";
