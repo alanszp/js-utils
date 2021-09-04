de5570ed18302c1858c2c5c3fad97e832cf5ada2
Alan Szpigiel <alanszp@gmail.com>
2021-09-04 12:18:39 -0300

    add common fn

--- packages/typeorm/src/index.ts
+++ packages/typeorm/src/index.ts
@@ -3,3 +3,4 @@ export * from "./BaseEntity";
 export * from "./BaseEntityWithoutId";
 export * from "./BaseModel";
 export * from "./ModelValidationError";
+export * from "./entityOrValidationError";
