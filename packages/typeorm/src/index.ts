4c345b41065be3fc4cf94cbab35beed0c9a5b0f8
Alan Szpigiel <alanszp@gmail.com>
2021-09-20 15:47:30 -0300

    export entity

--- packages/typeorm/src/index.ts
+++ packages/typeorm/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./TypeormLogger";
 export * from "./BaseEntity";
+export * from "./BaseEntityUuid";
 export * from "./BaseEntityWithoutId";
 export * from "./BaseModel";
 export * from "./ModelValidationError";
