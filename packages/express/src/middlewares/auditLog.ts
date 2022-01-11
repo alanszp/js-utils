17d93da289d84deb63dbcc6163bdfcd9feeb18b7
Alan Szpigiel <alanszp@gmail.com>
2022-01-11 20:12:40 -0300

    execute audit log after request sent but configure it before the controller

--- packages/express/src/middlewares/auditLog.ts
+++ packages/express/src/middlewares/auditLog.ts
@@ -1,7 +1,4 @@
-import { UnauthorizedError } from "@alanszp/errors";
-import { errorView } from "../views/errorView";
 import { NextFunction, Request, Response } from "express";
-import { getRequestLogger } from "../helpers/getRequestLogger";
 import { AuditBody } from "@alanszp/audit";
 import { getIp } from "../helpers/getIp";
 
@@ -10,38 +7,40 @@ export type AuditBodyModifier = (
   res: Response
 ) => Promise<Partial<AuditBody>> | Partial<AuditBody>;
 
-export function auditLog(
-  action: string,
-  bodyModifier?: AuditBodyModifier | void
-) {
-  return async function writeAuditLog(
+/**
+ * Configures the audit log to execute AFTER the response is sent.
+ */
+export function auditLog(action: string, bodyModifier?: AuditBodyModifier) {
+  return function writeAuditLogMiddleware(
     req: Request,
     res: Response,
     next: NextFunction
-  ): Promise<void> {
-    try {
-      const audit = req.context.audit;
+  ): void {
+    res.on("finish", async function writeAuditLog() {
+      try {
+        const audit = req.context.audit;
 
-      const partialBody = bodyModifier
-        ? await Promise.resolve(bodyModifier(req, res))
-        : ({} as Partial<AuditBody>);
+        const partialBody = bodyModifier
+          ? await Promise.resolve(bodyModifier(req, res))
+          : ({} as Partial<AuditBody>);
 
-      if (req.context.jwtUser) {
-        partialBody.orgRef = req.context.jwtUser.organizationReference;
-        partialBody.actorRef = req.context.jwtUser.id;
+        if (req.context.jwtUser) {
+          partialBody.orgRef = req.context.jwtUser.organizationReference;
+          partialBody.actorRef = req.context.jwtUser.id;
+        }
+
+        partialBody.ip = getIp(req) || "no-ip";
+
+        audit.log({
+          succeed: res.statusCode >= 200 && res.statusCode < 300,
+          ...partialBody,
+          action,
+        });
+      } catch (error: unknown) {
+        req.context.log.error("auditLog.writeLog.error", { action, error });
       }
+    });
 
-      partialBody.ip = getIp(req) || "no-ip";
-
-      audit.log({
-        succeed: res.statusCode >= 200 && res.statusCode < 300,
-        ...partialBody,
-        action,
-      });
-      next();
-    } catch (error: unknown) {
-      req.context.log.error("auditLog.writeLog.error", { action, error });
-      next();
-    }
+    next();
   };
 }
