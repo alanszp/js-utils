8812e150d217fab6d7c512c1cb8022f11ac272f2
fedekiwo <fedekiwo@gmail.com>
2022-10-13 22:17:15 +0200

    feat: middleware

---
+++ packages/serverless/src/middlewares/initializeSplitMiddleware.ts
@@ -0,0 +1,19 @@
+import type { BaseSplitClient } from "@alanszp/split";
+import { MiddlewareObj } from "@middy/core";
+import { SplitCouldNotLoadError } from "../errors/splitCouldNotLoadError";
+
+export function initializeSplitMiddleware<TEvent, TResult>(
+  splitClient: BaseSplitClient
+): MiddlewareObj<TEvent, TResult> {
+  return {
+    before: async (): Promise<void> => {
+      const loaded = await splitClient.hasLoaded();
+      if (!loaded) {
+        throw new SplitCouldNotLoadError();
+      }
+    },
+    after: async (): Promise<void> => {
+      await splitClient.destroy();
+    },
+  };
+}
