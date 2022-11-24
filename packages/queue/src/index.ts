87da3a1255de795e2eba732441c38ea92b8367eb
Federico Cano <federico.cano@meetlara.ai>
2022-11-24 11:05:02 -0300

    Rename package to queue

--- packages/queue/src/index.ts
+++ packages/queue/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./queue/createQueue";
 export * from "./connectionManager";
-export * from "./shutdown";
+export * from "./shutdownQueue";
 export * from "./types";
 export * from "./runner";
 export * from "./worker/worker";
--- packages/queue/src/shutdownQueue.ts
+++ packages/queue/src/shutdownQueue.ts
@@ -2,7 +2,7 @@ import { QueueRepository } from "./queue/queueRepository";
 import { ConnectionManager } from "./connectionManager";
 import { WorkerRepository } from "./worker/workerRepository";
 
-export async function shutdown(): Promise<void> {
+export async function shutdownQueue(): Promise<void> {
   await Promise.all([
     ...WorkerRepository.Instance.getCloseConnections(),
     ...QueueRepository.Instance.getCloseConnections(),
