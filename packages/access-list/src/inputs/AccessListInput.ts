2dcf6b99140b9714edb7e7f3dc2d9caee4e60f5a
Julian Murphy <njmdistrisoft@gmail.com>
2024-02-29 10:04:10 -0300

    export

---
+++ packages/access-list/src/inputs/AccessListInput.ts
@@ -0,0 +1,18 @@
+import { AccessListClient } from "../clients/AccessListClient";
+import { JWTUser } from "@alanszp/jwt";
+import { BaseModel } from "@alanszp/validations";
+import { IsDefined } from "class-validator";
+
+export class AccessListInput extends BaseModel {
+  @IsDefined()
+  public user: JWTUser;
+
+  constructor(user: JWTUser) {
+    super();
+    this.user = user;
+  }
+
+  public getAccessList(shouldAddFormerEmployees?: boolean): AccessListClient {
+    return new AccessListClient(this.user, shouldAddFormerEmployees);
+  }
+}
