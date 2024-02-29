266a6144a5e294216dd41cb5f7390e645a188f0c
Julian Murphy <njmdistrisoft@gmail.com>
2024-02-29 16:38:57 -0300

    dependencies

--- packages/access-list/src/index.ts
+++ packages/access-list/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./inputs/AccessListInput";
 export * from "./clients/AccessListClient";
 export * from "./repositories/accessListRepository";
+export * from "./errors//NoPermissionError";
