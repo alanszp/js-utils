cbb6fe2c70d19b3a55e8ddbe88b5333132c233ac
Alan Szpigiel <alanszp@gmail.com>
2024-05-30 11:53:26 -0300

    add better interface

--- packages/queue/src/shutdownQueue.ts
+++ packages/queue/src/shutdownQueue.ts
@@ -1,3 +1,4 @@
+import { isArray } from "lodash";
 import { ConnectionManager } from "./connectionManager";
 import { QueueManager } from "./queue/QueueManager";
 import { BaseQueue } from "./queue/baseQueue";
@@ -7,10 +8,18 @@ export async function shutdownQueue<
   EnumKey extends string | number | symbol,
   EnumValue extends string,
   QueueType extends BaseQueue
->(queueManagers: QueueManager<EnumKey, EnumValue, QueueType>[]): Promise<void> {
+>(
+  queueManagers:
+    | QueueManager<EnumKey, EnumValue, QueueType>
+    | QueueManager<EnumKey, EnumValue, QueueType>[]
+): Promise<void> {
+  const queueManagersArray = isArray(queueManagers)
+    ? queueManagers
+    : [queueManagers];
+
   await Promise.all([
     ...WorkerRepository.Instance.getCloseConnections(),
-    ...queueManagers.map((q) => q.disconnectAll()),
+    ...queueManagersArray.map((q) => q.disconnectAll()),
   ]);
 
   ConnectionManager.getInstance().close();
