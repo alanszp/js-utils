58e926815fcc8be105822b2d088cb4335a4e405d
Alan Szpigiel <alanszp@gmail.com>
2024-06-26 14:07:02 -0300

    Update packages/audit/src/interfaces.ts
    Co-authored-by: Federico Kiwowicz <fedekiwo@gmail.com>

--- packages/audit/src/interfaces.ts
+++ packages/audit/src/interfaces.ts
@@ -4,7 +4,7 @@ export interface AuditBody {
   orgRef: string; // Original org from the real user.
 
   impersonatedActorRef?: string; // Impersonated user ID. If no impersonating, it will be undefined
-  impersonatedOrgRef?: string; // Org that was accessed. If no impersonating, it will be the same as orgRef
+  impersonatedOrgRef?: string; // Org that was accessed. If no impersonating, it will be undefined
 
   // Where (it can be an array since the request can jump between nodes, and http keeps record of their IPs)
   ip: string | string[];
